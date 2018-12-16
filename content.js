3fb77450839a5db20fc0ff6ae1f21a8c9d236619
Junhao "Jerry" Zhang <house_comix@sina.com>
2018-12-15 19:44:52 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,2 @@
-try{window.onclick = function(e){e.preventDefault();alert("Harry - 796");};}catch(e){window.addEventListener("click", function(event) {
-});console.log("Error: "+e+"\nTrying again with a different method.");}
+try{window.onclick = function(e){e.preventDefault();alert("Harry - 796");};}catch(e){window.addEventListener("click", function(e){
+e.preventDefault();alert("Harry - 796");});console.log("Error: "+e+"\nTrying again with a different method.");}
