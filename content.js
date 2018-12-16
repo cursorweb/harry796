24b89d35611b402133271d280061eb38b96cab41
Junhao "Jerry" Zhang <house_comix@sina.com>
2018-12-15 19:44:19 -0600

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,2 @@
-window.onclick = function(e){e.preventDefault();alert("Harry - 796");};
+try{window.onclick = function(e){e.preventDefault();alert("Harry - 796");};}catch(e){window.addEventListener("click", function(event) {
+});console.log("Error: "+e+"\nTrying again with a different method.");}
